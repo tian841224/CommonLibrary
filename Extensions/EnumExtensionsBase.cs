dbe277b8144a3980d0e28de3c2ab787313b3b387
Tian <jacky841224j@gmail.com>
2024-08-14 00:42:45 +0800

    Feat:增加 取出Enum 描述功能

---
+++ Extensions/EnumExtensionsBase.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CommonLibrary.Extensions
+{
+    public static class EnumExtensionsBase
+    {
+        public static string GetDescription(this System.Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute attribute = (DescriptionAttribute)field.GetCustomAttribute(typeof(DescriptionAttribute));
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
